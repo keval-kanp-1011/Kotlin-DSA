7a9bc17916bf73c335fd3ccaafd5907bd49b0874
Keval <kevalkanpariya5051@gmail.com>
2023-10-05 23:56:03 +0530

    Problems Added 1. Find A Pick Element from an Array 2. Sqrt Root of any Int Number

---
+++ src/main/kotlin/FindAPickElement.kt
@@ -0,0 +1,62 @@
+
+/**
+ *BruteForce Approach
+ */
+fun findAPickElement(array: IntArray) : Int {
+
+
+    //for only one peak
+    for (i in array.indices) {
+
+        if ((i == 0 || array[i-1] < array[i]) && (i == array.lastIndex || array[i] > array[i+1])) {
+            return i
+        }
+    }
+
+
+    return -1
+}
+
+/**
+ * Optimized Approach
+ */
+fun findAPickElementOptimized(array: IntArray): Int {
+
+
+    //if multiple peak is available return any one of the peak.
+    if (array.size == 1) return 0
+
+    if (array[0] > array[1]) return 0
+
+    if (array[array.lastIndex] > array[array.lastIndex - 1]) return  array.lastIndex
+
+    var low = 0
+    var high = array.lastIndex - 2
+
+    while (low <= high) {
+        val mid = (low+high)/2
+
+
+        if ( array[mid] > array[mid-1] && array[mid] > array[mid+1]) {
+            return mid
+        } else if (array[mid] > array[mid - 1]) {
+            low = mid+1
+        } else {
+            //two cases are considered
+            //1. mid is on the Right Arm
+            //2. array[mid] < array[mid-1] && array[mid] < array[mid+1] (minima point)
+            high = mid - 1
+        }
+
+    }
+
+    return  -1
+}
+
+fun main() {
+
+    val array = intArrayOf(1,2,3,4,10,6,7,8,5,1)
+    val array2 = intArrayOf(1)
+    println(findAPickElementOptimized(array))
+    println(findAPickElement(array2))
+}
---
+++ src/main/kotlin/SqrtNoBinarySearch.kt
@@ -0,0 +1,46 @@
+
+/**
+ *Bruteforce Approach
+ */
+fun sqrtNoBinarySearch(n: Int): Int {
+
+    var ans = 0
+    for( i in 1..n) {
+
+        if (i*i <= n) {
+            ans = i
+        } else {
+            break
+        }
+    }
+
+    return ans
+}
+
+/**
+ *Optimum Approach
+ */
+fun sqrtNoBinarySearchOpt(n: Int): Int {
+
+    var low = 1
+    var high = n
+    var ans = 0
+    while (low <= high) {
+
+        val mid = (low+high)/2
+
+        if (mid*mid <= n) {
+            ans = mid
+            low = mid+1
+        } else {
+            high = mid -1
+        }
+    }
+    return ans
+}
+
+fun main() {
+
+    println(sqrtNoBinarySearchOpt(25))
+    println(sqrtNoBinarySearch(25))
+}
