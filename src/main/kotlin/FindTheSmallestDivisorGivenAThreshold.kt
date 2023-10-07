1dde098bfb77117510f14fdb99d460b658f400a0
Keval <kevalkanpariya5051@gmail.com>
2023-10-07 23:22:59 +0530

    Problems Added 1. FindTheSmallestDivisorGivenAThreshold

---
+++ src/main/kotlin/FindTheSmallestDivisorGivenAThreshold.kt
@@ -0,0 +1,41 @@
+import kotlin.math.ceil
+
+fun sumOfD(array: IntArray, div: Int): Int {
+
+    var sum = 0
+
+    for (i in array) {
+        sum += ceil(i.toDouble() / div.toDouble()).toInt()
+    }
+
+    return sum
+
+}
+fun smallestDivisor(array: IntArray, limit: Int): Int {
+
+    var low = 1
+    var high = array.maxOrNull()!!
+
+    if (array.size > limit) return -1
+
+    while (low <= high) {
+
+        val mid = (low+high)/2
+
+        if (sumOfD(array, mid) <= limit) {
+            high = mid -1
+        } else {
+            low = mid + 1
+        }
+    }
+
+    return low
+
+}
+
+fun main() {
+
+    val array = intArrayOf(1,2,3,4,5)
+    println(smallestDivisor(array, 8))
+
+}
