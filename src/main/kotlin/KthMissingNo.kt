b2d9df53b989dcb435acec4f4133d54eb5be8854
Keval <kevalkanpariya5051@gmail.com>
2023-10-12 01:00:31 +0530

    Problems Added 1.KthMissingNo

---
+++ src/main/kotlin/KthMissingNo.kt
@@ -0,0 +1,30 @@
+
+fun missingK(array: IntArray, k: Int): Int {
+
+    var low = 0
+    var high = array.lastIndex
+
+    while (low <= high) {
+
+        val mid =(low+high)/2
+
+        val missing = array[mid] - (mid+1)
+
+        if (missing < k) {
+            low = mid +1
+        } else {
+            high = mid -1
+        }
+
+    }
+
+    return high + 1 + k
+
+}
+
+
+fun main() {
+    val array = intArrayOf(2,3,4,7,11)
+
+    println(missingK(array, 6))
+}
