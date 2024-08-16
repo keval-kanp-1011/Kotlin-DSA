4e9e5c53523cf8a526358c2382673342d3369b26
Keval <kevalkanpariya5051@gmail.com>
2024-08-16 11:07:29 +0530

    InsertionSort and MergeSort Code Added.

--- src/main/kotlin/sorting/InsertionSort.kt
+++ src/main/kotlin/sorting/InsertionSort.kt
@@ -1,2 +1,36 @@
 package sorting
 
+fun insertionSort(
+    array: IntArray
+) {
+
+
+    for(i in 1..<array.size) {
+
+        val temp = array[i]
+
+        var j = i-1
+        while (j >= 0 && array[j] > temp) {
+            array[j+1] = array[j]
+            j--
+        }
+        if (array[j+1] != temp) {
+            array[j+1] = temp
+        }
+
+
+
+    }
+}
+
+
+fun main() {
+
+    val sample: IntArray = intArrayOf(8,4,1,5,9,2)
+    insertionSort(sample)
+    sample.forEach {
+        print("$it ")
+    }
+
+
+}
--- src/main/kotlin/sorting/MergeSort.kt
+++ src/main/kotlin/sorting/MergeSort.kt
@@ -1,2 +1,61 @@
 package sorting
 
+fun mergeSort(array: IntArray, l: Int, r: Int) {
+
+
+    if (l < r){
+        val mid =  (l+r)/2
+        mergeSort(array,l, mid)
+        mergeSort(array, mid+1, r)
+        merge(array, l, mid, r)
+    }
+}
+
+
+fun merge(array: IntArray, l: Int, mid: Int, r: Int) {
+
+    var i: Int = l
+    var j: Int = mid+1
+    var k: Int = l
+
+    val b = arrayOfNulls<Int>(array.size)
+    while (i <= mid && j <= r) {
+        if (array[i] > array[j]) {
+            b[k] = array[j]
+            j++
+        } else {
+            b[k] = array[i]
+            i++
+        }
+        k++
+    }
+
+    if (i > mid) {
+        while (j <= r) {
+            b[k] = array[j]
+            j++
+            k++
+        }
+    } else {
+        while (i <= mid) {
+            b[k] = array[i]
+            i++
+            k++
+        }
+    }
+
+    for (k in l..r) {
+        array[k] = b[k]!!
+    }
+}
+
+
+fun main() {
+    val array = intArrayOf(8,4,1,5,9,2)
+
+    mergeSort(array, 0, array.size-1)
+
+    array.forEach {
+        print("$it ")
+    }
+}
